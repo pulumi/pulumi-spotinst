f408f8b8c00b249929419fc3b10ddf996c2bd8f4
Chris Smith <chrsmith@users.noreply.github.com>
2020-05-16 12:04:28 -0700

    Use new configure-aws.sh script for managing AWS keys (#20)

--- scripts/publish-plugin.sh
+++ scripts/publish-plugin.sh
@@ -27,43 +27,30 @@ fi
 # Tar up the plugin
 tar -czf ${PLUGIN_PACKAGE_PATH} -C ${WORK_PATH} .
 
-# Store the initial AWS credentials. The AWS creds will get overwritten after
-# calling assume_iam_role, and restored via restore_initial_aws_keys.
-export INITIAL_AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
-export INITIAL_AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
-
-restore_initial_aws_keys() {
-    unset {AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY,AWS_SECURITY_TOKEN}
-    export AWS_ACCESS_KEY_ID="${INITIAL_AWS_ACCESS_KEY_ID}"
-    export AWS_SECRET_ACCESS_KEY="${INITIAL_AWS_SECRET_ACCESS_KEY}"
-}
-
-assume_iam_role() {
-    local CREDS_JSON=$(aws sts assume-role \
-                 --role-arn "${1}" \
-                 --role-session-name "upload-plugin-pulumi-resource-${PROVIDER_NAME}" \
-                 --external-id "upload-pulumi-release")
-    export AWS_ACCESS_KEY_ID=$(echo ${CREDS_JSON}     | jq ".Credentials.AccessKeyId" --raw-output)
-    export AWS_SECRET_ACCESS_KEY=$(echo ${CREDS_JSON} | jq ".Credentials.SecretAccessKey" --raw-output)
-    export AWS_SECURITY_TOKEN=$(echo ${CREDS_JSON}    | jq ".Credentials.SessionToken" --raw-output)
-}
-
+# Switch back to the default AWS profile for the duration of this script. Since if we were using
+# another profile, we wouldn't be able to assume the upload roles. (Functions defined in
+# scripts repo in ci/configure-aws.sh, sourced from .travis.yml.)
+unset AWS_PROFILE
 
 echo "Uploading ${PLUGIN_PACKAGE_NAME} to s3://rel.pulumi.com..."
-
-assume_iam_role  "arn:aws:iam::058607598222:role/UploadPulumiReleases"
+assume_iam_role \
+     "arn:aws:iam::058607598222:role/UploadPulumiReleases" \
+     "upload-plugin-pulumi-resource-${PROVIDER_NAME}" \
+     "upload-pulumi-release"
 aws s3 cp --only-show-errors "${PLUGIN_PACKAGE_PATH}" "s3://rel.pulumi.com/releases/plugins/${PLUGIN_PACKAGE_NAME}"
-restore_initial_aws_keys
+unassume_iam_role
 
 # Assume the role to publish plugins to s3://get.pulumi.com. We upload the plugins to two buckets while
 # we transition to only publishing/serving them from get.pulumi.com.
 echo "Uploading ${PLUGIN_PACKAGE_NAME} to s3://get.pulumi.com..."
-
-assume_iam_role "arn:aws:iam::058607598222:role/PulumiUploadRelease"
+assume_iam_role \
+     "arn:aws:iam::058607598222:role/PulumiUploadRelease" \
+     "upload-plugin-pulumi-resource-${PROVIDER_NAME}" \
+     "upload-pulumi-release"
 aws s3 cp \
     --only-show-errors --acl public-read \
     "${PLUGIN_PACKAGE_PATH}" "s3://get.pulumi.com/releases/plugins/${PLUGIN_PACKAGE_NAME}"
-restore_initial_aws_keys
+unassume_iam_role
 
 rm -rf "${PLUGIN_PACKAGE_DIR}"
 rm -rf "${WORK_PATH}"
