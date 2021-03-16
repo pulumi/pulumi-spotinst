a6be6128fa53db3788ac93e766916a046554cd17
Paul Stack <public@paulstack.co.uk>
2021-03-16 11:35:02 +0000

    Upgrading to pulumi-terraform-bridge v2.21.0

--- sdk/python/pulumi_spotinst/gcp/elastigroup.py
+++ sdk/python/pulumi_spotinst/gcp/elastigroup.py
@@ -109,7 +109,7 @@ class Elastigroup(pulumi.CustomResource):
             __props__['auto_healing'] = auto_healing
             if availability_zones is not None and not opts.urn:
                 warnings.warn("""This field will soon be handled by Region in Subnets""", DeprecationWarning)
-                pulumi.log.warn("availability_zones is deprecated: This field will soon be handled by Region in Subnets")
+                pulumi.log.warn("""availability_zones is deprecated: This field will soon be handled by Region in Subnets""")
             __props__['availability_zones'] = availability_zones
             __props__['backend_services'] = backend_services
             __props__['description'] = description
